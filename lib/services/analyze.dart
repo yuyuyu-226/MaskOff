ef7620ce7d3d2127b07a198bc598c9955a94bde0
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2026-02-01 03:18:58 +0800

    Added VPN

--- lib/services/analyze.dart
+++ lib/services/analyze.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 Future<String> analyzeEmotion(String userInput) async {
   // Use 10.0.2.2 if using an Android Emulator, otherwise use your PC IP
-  final String url = 'http://172.15.8.89:8000/analyze'; 
+  final String url = 'http://172.27.4.119:8000/analyze'; 
 
   try {
     final response = await http.post(
